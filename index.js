272ac1faba6c6fc23448069bd3e192a6e444b8c4
Muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-07-11 23:38:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+  // Attempting to change a constant will result in an error (TypeError).
+  leastFavoriteCustomer = 'Bob';
+}
+n
